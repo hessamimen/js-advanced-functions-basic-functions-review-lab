6ded783a1aba1b95a860f9ee8bd9d6c763a99d57
Hessam Imen <hessam.imen@gmail.com>
2022-03-02 20:05:02 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(workActivity = "go to the office"){
+    return `This Monday, I will ${workActivity}.`
+}
+
+function wrapAdjective(adj ="*"){
+    return function(value = "special"){
+        return `You are ${adj}${value}${adj}!`;
+    }
+}
+let Calculator = {
+    add: function(){
+        return 1+3;
+    },
+    subtract: function(){
+        return 1-3;
+    },
+    multiply: function(){
+        return 1*3;
+    },
+    divide: function(){
+        return 10/5;
+    },
+}
+// function actionApplyer(start, fnArr){
+//     fnArr = [
+//         function(start){ return a * 2 },
+//         function(start){ return a + 1000},
+//         function(start){ return a % 7 }
+//       ]
+// }
+
+function actionApplyer(start, fnArr) {
+    for (let i = 0; i < fnArr.length; i++) {
+      start = fnArr[i](start);
+    }
+    return start;
+  }
+
+  console.log(actionApplyer(13, [
+                function(start){ return start * 2 },
+                function(start){ return start + 1000},
+                function(start){ return start % 7 }
+              ]));
+  
